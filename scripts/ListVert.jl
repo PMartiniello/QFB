894b3a5ee403f732bb5b3a0d261325067630eda9
Paolo Martiniello <120146356+PMartiniello@users.noreply.github.com>
2025-01-14 16:15:48 -0300

    update in functions

---
+++ scripts/ListVert.jl
@@ -0,0 +1,13 @@
+function listvert(L)
+    n = length(L)
+    char = String[]
+    for i in 1:n
+        # Dividir la cadena en caracteres y convertirlos a String
+        a, b, c = map(String, split(L[i], ""))
+        push!(char, a)
+        push!(char, b)
+        push!(char, c)
+    end
+    char = unique(char)  # Eliminar caracteres repetidos
+    return char
+end
