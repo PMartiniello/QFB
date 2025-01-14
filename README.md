090e7ae3a319255a23f46a78ec48b7c528f317b1
Paolo Martiniello <120146356+PMartiniello@users.noreply.github.com>
2025-01-14 16:03:12 -0300

    Small changes in README and Format.jl for asserting the argument.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Quasimetric-from-betweenness
 QFB.jl is a package made to help study the discrete quasimetric spaces spanned from betweenness.
-
+\\
 For a certain betweenness $B$ the quasimetric $Q(B)$ that we study is the one that satisfies the following ILP:
+\\
 $
 \begin{align}
     \min \ & C\\
@@ -12,6 +13,7 @@ $
     & Q_{x,y} \in \mathbb{N}
 \end{align}
 $
+\\
 A matrix $Q(B)$ that satisfies this will be a representation of the quasimetric with betweenness $B$ and the minimal norm $||Q||_1 = \sum_{x,\ y \in V}Q_{x,y}$.
 
 In case $Q(B)$ exists it will also be represented as a weighted digraph, using the least arcs possible.
--- Scripts/Format.jl
+++ Scripts/Format.jl
@@ -1,12 +1,17 @@
 using Combinatorics
+using ArgCheck
 
 function format(B::Vector{String})
     # Recibe la betweenness como una lista con str de largo 3
     # Devuelve betweenness formateada con 3 str por elemento de la lista y los tríos que no están en la betweenness
+    for b in B
+        @argcheck(length(b) == 3)
+    end
+
     v = listvert(B)
     
     # Genera la betweenness formateada
-    F = []
+    F = Vector{String}[]
     for b in B
         # Convertimos SubString a String con map(String, ...)
         x, y, z = map(String, split(b, ""))
@@ -14,7 +19,7 @@ function format(B::Vector{String})
     end
     
     # Todas las permutaciones de tríos de vértices
-    T = []
+    T = Vector{String}[]
     for p in permutations(v, 3)
         push!(T, p)
     end
