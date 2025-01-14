7fc47a3efe8bd5527829cc23626ba5e86492372b
Paolo Martiniello <120146356+PMartiniello@users.noreply.github.com>
2025-01-14 15:09:36 -0300

    Created README.md and uploaded code for Clp to work.

---
+++ Scripts/Format.jl
@@ -0,0 +1,28 @@
+using Combinatorics
+
+function format(B::Vector{String})
+    # Recibe la betweenness como una lista con str de largo 3
+    # Devuelve betweenness formateada con 3 str por elemento de la lista y los tríos que no están en la betweenness
+    v = listvert(B)
+    
+    # Genera la betweenness formateada
+    F = []
+    for b in B
+        # Convertimos SubString a String con map(String, ...)
+        x, y, z = map(String, split(b, ""))
+        push!(F, [x, y, z])
+    end
+    
+    # Todas las permutaciones de tríos de vértices
+    T = []
+    for p in permutations(v, 3)
+        push!(T, p)
+    end
+    
+    # Quita los que están en la betweenness
+    for b in F
+        T = filter!(n -> n != b, T)
+    end
+    
+    return F, T
+end
